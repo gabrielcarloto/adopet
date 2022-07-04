ead7a0b6dcf0381f1326b5a018a5234cc8df039d
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-04 15:23:49 -0300

    ✨ feat: add top-left shape

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      backgroundSize: {
+        '95%': '95%',
+      },
+      backgroundImage: {
+        'shape-1': 'url(/src/assets/shape-1.svg)',
+        'shape-2': 'url(/src/assets/shape-2.svg)',
+      },
       fontFamily: {
         sans: 'Poppins, sans-serif',
       },
