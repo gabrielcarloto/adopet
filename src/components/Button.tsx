ba29f4fb0353e89485cb15fbfa00a938e02934ba
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-05 18:54:41 -0300

    ✨ feat: add option to render Button as anchor

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,38 @@
+import { Link } from 'react-router-dom';
+
 interface ButtonProps {
   size: 'md' | 'lg';
   text: string;
+  as?: 'button' | 'a';
+  href?: string;
 }
 
-export default function Button({ size, text }: ButtonProps) {
+export default function Button({
+  size,
+  text,
+  as = 'button',
+  href,
+}: ButtonProps) {
   const width =
     size === 'md'
       ? 'w-[148px] md:w-[164px] xl:w-[174px]'
       : 'w-[180px] md:w-[344px] xl:w-[362px]';
 
+  const As = ({ classes }: { classes: string }) => {
+    if (as === 'a') {
+      return (
+        <Link className={classes} to={href!}>
+          {text}
+        </Link>
+      );
+    } else {
+      return <button className={classes}>{text}</button>;
+    }
+  };
+
   return (
-    <button
-      className={`bg-brand-tertiary h-10 md:h-12 rounded-md text-white text-base md:text-lg font-semibold ${width}`}
-    >
-      {text}
-    </button>
+    <As
+      classes={`bg-brand-tertiary h-10 md:h-12 grid place-items-center rounded-md text-white text-base md:text-lg font-semibold ${width}`}
+    />
   );
 }
