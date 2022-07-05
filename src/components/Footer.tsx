fb7f6cb441e329bf6eb64b0758fb17efc25d2726
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-04 21:27:14 -0300

    ♻️ refactor: remove `mt-auto` from Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="w-full h-[104px] lg:h-20 mt-auto grid place-items-center bg-brand-secondary">
+    <footer className="w-full h-[104px] lg:h-20 grid place-items-center bg-brand-secondary">
       <p className="text-white font-normal text-base">
         2022 - Desenvolvido por Gabriel Carloto.
       </p>
