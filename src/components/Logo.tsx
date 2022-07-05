b0f8005f08867e9fb825b566d283dde3762be64c
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-05 19:50:41 -0300

    ✨ feat(logo): add classes prop

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,21 @@
+import classNames from 'classnames';
 import LogoBlue from '../assets/logo-blue.svg';
 import LogoCream from '../assets/logo-cream.svg';
 
 interface LogoProps {
   variant: 'cream' | 'blue';
+  classes?: string;
 }
 
-export default function Logo({ variant }: LogoProps) {
+export default function Logo({ variant, classes }: LogoProps) {
   const source = variant === 'cream' ? LogoCream : LogoBlue;
 
   return (
     <img
-      className="w-48 md:w-64 xl:w-[260px] md:mt-2"
+      className={classNames({
+        'w-48 md:w-64 xl:w-[260px] md:mt-2': !classes,
+        [classes!]: classes,
+      })}
       src={source}
       alt="Logo da adopet"
     />
