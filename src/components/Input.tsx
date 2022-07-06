1ebd25220ab36ea4bc10126e6874a020b1de011c
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-06 15:21:56 -0300

    ✨ feat(input): add password visibility button animated with lottie

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,8 @@
+import { CSSProperties, HTMLInputTypeAttribute, useId, useState } from 'react';
 import classNames from 'classnames';
-import { HTMLInputTypeAttribute, useId } from 'react';
+import { LottieOptions, useLottie } from 'lottie-react';
+
+import visibilityAnimation from '../assets/visibility.json';
 
 interface InputProps {
   type: HTMLInputTypeAttribute;
@@ -10,6 +13,16 @@ interface InputProps {
   maxLength?: number;
 }
 
+const lottieStyle: CSSProperties = {
+  height: 24,
+};
+
+const lottieOptions: LottieOptions = {
+  animationData: visibilityAnimation,
+  autoplay: false,
+  loop: false,
+};
+
 export default function Input({
   type,
   label,
@@ -18,9 +31,28 @@ export default function Input({
   minLength,
   maxLength,
 }: InputProps) {
+  const [isPassVisible, setIsPassVisible] = useState<null | boolean>(
+    type === 'password' ? false : null,
+  );
+
   const inputId = useId();
 
+  const LottieVisibility = useLottie(lottieOptions, lottieStyle);
+
   const isTextOrEmail = type === 'text' || type === 'email';
+  const isPassword = isPassVisible || isPassVisible === false;
+
+  function handleChangePassVisibility() {
+    setIsPassVisible(!isPassVisible);
+
+    LottieVisibility.play();
+  }
+
+  if (isPassVisible) {
+    LottieVisibility.setDirection(1);
+  } else {
+    LottieVisibility.setDirection(-1);
+  }
 
   return (
     <div className="flex flex-col items-center gap-2">
@@ -30,26 +62,43 @@ export default function Input({
       >
         {label}
       </label>
-      <input
-        className={classNames(
-          [
-            'w-full h-10 p-2 rounded-md bg-brand-gray-50 shadow-sm text-brand-gray-500 text-center',
-            'placeholder:text-center placeholder:text-xs md:placeholder:text-sm xl:placeholder:text-base',
-            'placeholder:text-brand-gray-300 xl:p-3 focus:ring-2 focus:ring-brand-primary ring-offset-2',
-            'outline-none',
-          ],
-          {
-            'xl:w-[552px]': isTextOrEmail,
-            'xl:w-[362px]': !isTextOrEmail,
-          },
+      <div className="relative grid place-items-center">
+        <input
+          className={classNames(
+            [
+              'h-10 rounded-md bg-brand-gray-50 shadow-sm text-brand-gray-500 text-center',
+              'placeholder:text-center placeholder:text-xs md:placeholder:text-sm xl:placeholder:text-base',
+              'placeholder:text-brand-gray-300 focus:ring-2 focus:ring-brand-primary ring-offset-2',
+              'outline-none',
+            ],
+            {
+              'xl:w-[552px]': isTextOrEmail,
+              'xl:w-[362px]': !isTextOrEmail && !isPassword,
+              'p-2 xl:p-3': !isPassword,
+              'pl-2 pr-10 xl:pl-3 xl:pr-12 w-[calc(100%-32px)] xl:w-[318px]':
+                isPassword,
+            },
+          )}
+          id={inputId}
+          type={isPassword ? (isPassVisible ? 'text' : 'password') : type}
+          placeholder={placeholder}
+          required={required}
+          minLength={minLength}
+          maxLength={maxLength}
+        />
+        {isPassword && (
+          <button
+            className="absolute right-5 xl:right-1 top-2"
+            type="button"
+            onClick={handleChangePassVisibility}
+            title={`Alterar a visibilidade da senha. Agora a senha está ${
+              isPassVisible ? 'visível' : 'escondida'
+            }`}
+          >
+            {LottieVisibility.View}
+          </button>
         )}
-        id={inputId}
-        type={type}
-        placeholder={placeholder}
-        required={required}
-        minLength={minLength}
-        maxLength={maxLength}
-      />
+      </div>
     </div>
   );
 }
