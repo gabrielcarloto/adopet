9c0fe40db8b14e7f9fe37b474f6cd2c8ac54c04b
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-07 17:12:44 -0300

    🩹 fix(pets): align h1 text center

--- src/pages/Pets.tsx
+++ src/pages/Pets.tsx
@@ -3,7 +3,7 @@ import Pet from '../components/Pet';
 export default function Pets() {
   return (
     <main className="flex-auto flex flex-col pt-[72px] md:pt-[188px] md:pb-8 xl:pt-[102px] xl:pb-[60px] container">
-      <h1 className="mx-auto mb-8 xl:mb-10 max-w-[226px] md:max-w-[373px] xl:max-w-full text-brand-primary md:text-lg">
+      <h1 className="mx-auto mb-8 xl:mb-10 max-w-[226px] md:max-w-[373px] xl:max-w-full text-brand-primary text-center md:text-lg">
         Olá! Veja os amigos disponíveis para adoção!
       </h1>
 
