2e6138da1a3adb6743b979bfc8824f07b17b271a
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-07 20:51:49 -0300

    ✨ feat: create Popover component

---
+++ src/components/Header/Popover.tsx
@@ -0,0 +1,77 @@
+import { Popover as HeadlessUIPopover, Transition } from '@headlessui/react';
+import { ReactNode, SyntheticEvent, useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+
+interface KeyboardEvent<T = Element> extends SyntheticEvent<T> {
+  altKey: boolean;
+  /** @deprecated */
+  charCode: number;
+  ctrlKey: boolean;
+  getModifierState(key: string): boolean;
+  key: string;
+  /** @deprecated */
+  keyCode: number;
+  locale: string;
+  location: number;
+  metaKey: boolean;
+  repeat: boolean;
+  shiftKey: boolean;
+  /** @deprecated */
+  which: number;
+}
+
+interface PopoverProps {
+  label: string;
+  to: string;
+  children: ReactNode;
+}
+
+export default function Popover({ label, to, children }: PopoverProps) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+  useEffect(() => setIsPanelOpen(false), [location]);
+
+  return (
+    <HeadlessUIPopover
+      className="relative"
+      onMouseEnter={() => setIsPanelOpen(true)}
+      onMouseLeave={() => setIsPanelOpen(false)}
+      onFocus={() => setIsPanelOpen(true)}
+      onBlur={() => setIsPanelOpen(false)}
+    >
+      <HeadlessUIPopover.Button
+        as="div"
+        onKeyDown={(e: KeyboardEvent<HTMLDivElement>) => {
+          /**
+           * for some reason, depending on where the Link component is,
+           * it either stops working when clicked or when pressing enter,
+           * so I did this workaround
+           */
+          if (e.key === 'Enter') navigate(to);
+        }}
+      >
+        <Link to={to}>{children}</Link>
+      </HeadlessUIPopover.Button>
+
+      <Transition
+        show={isPanelOpen}
+        enter="transition-all duration-150"
+        enterFrom="opacity-0 absolute top-0 rotate-y-40"
+        enterTo="opacity-100 absolute top-0 rotate-y-0"
+        leave="transition-all duration-150"
+        leaveFrom="opacity-100 absolute top-0 rotate-y-0"
+        leaveTo="opacity-0 absolute top-0 rotate-y-40"
+      >
+        <HeadlessUIPopover.Panel
+          className="w-max p-1 absolute left-0 top-7 bg-white shadow-lg text-brand-gray-500 text-sm rounded"
+          static
+        >
+          <Link to={to}>{label}</Link>
+        </HeadlessUIPopover.Panel>
+      </Transition>
+    </HeadlessUIPopover>
+  );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,56 @@
+import { Link, useLocation } from 'react-router-dom';
+import { Menu } from '@headlessui/react';
+import Popover from './Popover';
+
+import { HomeIcon, MailIcon, UserIcon } from '../Icons';
+import Logo from '../Logo';
+
+export default function Header() {
+  const location = useLocation();
+
+  const isLoggedIn = true;
+  const routesUserShouldNotAppear =
+    location.pathname === '/' ||
+    location.pathname === '/cadastro' ||
+    location.pathname === '/login';
+
+  const shouldUserBeVisible = isLoggedIn && !routesUserShouldNotAppear;
+
+  return (
+    <header className="w-screen max-w-full mt-12 md:mt-16 pl-12 xl:pl-40 flex flex-initial items-center text-white">
+      <Popover label="Voltar à página inicial" to="/">
+        <Logo variant="cream" className="hidden md:block w-32" />
+      </Popover>
+
+      <div className="ml-12 flex gap-14 md:gap-16 xl:gap-8">
+        <Popover label="Ver amigos disponíveis para adoção" to="/pets">
+          <HomeIcon className="h-[23px]" aria-label="Pets" />
+        </Popover>
+
+        <Popover label="Ver mensagens" to="#">
+          <MailIcon className="h-[23px]" aria-label="Mensagens" />
+        </Popover>
+      </div>
+      {shouldUserBeVisible && (
+        <Menu>
+          <div className="relative flex-1 mr-10 xl:mr-44 grid place-items-end">
+            <Menu.Button>
+              <UserIcon className="w-10" aria-label="Usuário" />
+            </Menu.Button>
+            <Menu.Items className="absolute top-11 w-max rounded shadow-lg bg-white text-brand-primary text-sm flex flex-col items-center">
+              <Menu.Item>
+                <Link to="#" className="p-1">
+                  Perfil
+                </Link>
+              </Menu.Item>
+              {/* <hr className="w-full border-brand-primary" />
+            <Link to="#" className="p-1">
+              Outro link
+            </Link> */}
+            </Menu.Items>
+          </div>
+        </Menu>
+      )}
+    </header>
+  );
+}
