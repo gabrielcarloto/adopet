9261947d8695b1f247c35c64fbc93438e011336e
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-07 17:17:42 -0300

    ♻️ refactor(button): remove `As` component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,21 +18,15 @@ export default function Button({
       ? 'w-[148px] md:w-[164px] xl:w-[174px]'
       : 'w-[180px] md:w-[344px] xl:w-[362px]';
 
-  const As = ({ classes }: { classes: string }) => {
-    if (as === 'a') {
-      return (
-        <Link className={classes} to={href!}>
-          {text}
-        </Link>
-      );
-    } else {
-      return <button className={classes}>{text}</button>;
-    }
-  };
+  const classes = `bg-brand-tertiary hover:bg-brand-coral-100 focus:ring-2 focus:ring-brand-tertiary ring-offset-2 outline-none transition-colors h-10 md:h-12 grid place-items-center rounded-md text-white text-base md:text-lg font-semibold ${width}`;
 
-  return (
-    <As
-      classes={`bg-brand-tertiary hover:bg-brand-coral-100 focus:ring-2 focus:ring-brand-tertiary ring-offset-2 outline-none transition-colors h-10 md:h-12 grid place-items-center rounded-md text-white text-base md:text-lg font-semibold ${width}`}
-    />
-  );
+  if (as === 'a') {
+    return (
+      <Link className={classes} to={href!}>
+        {text}
+      </Link>
+    );
+  } else {
+    return <button className={classes}>{text}</button>;
+  }
 }
