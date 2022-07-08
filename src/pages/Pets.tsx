88175a461c2933520583785eea1ba1e1e8377758
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-08 10:16:07 -0300

    ♻️ refactor: render pets with array

--- src/pages/Pets.tsx
+++ src/pages/Pets.tsx
@@ -1,6 +1,8 @@
 import Pet from '../components/Pet';
 
 export default function Pets() {
+  const array = Array.from(Array(9));
+
   return (
     <main className="flex-auto flex flex-col m-header md:pb-8 xl:pb-[60px] container">
       <h1 className="mx-auto mb-8 xl:mb-10 max-w-[226px] md:max-w-[373px] xl:max-w-full text-brand-primary text-center md:text-lg">
@@ -8,17 +10,9 @@ export default function Pets() {
       </h1>
 
       <section className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
-        <Pet />
+        {array.map(() => (
+          <Pet />
+        ))}
       </section>
     </main>
   );
