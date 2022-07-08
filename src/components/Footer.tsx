3307bcc991fa18c6b3f3ba22ce059ef51d08491f
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-07-07 21:03:03 -0300

    ✨ feat(footer): add github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer className="w-full h-[104px] md:h-20 flex-initial grid place-items-center bg-brand-secondary">
       <p className="text-white font-normal text-base">
-        2022 - Desenvolvido por Gabriel Carloto.
+        2022 - Desenvolvido por{' '}
+        <a href="https://github.com/gabrielcarloto">Gabriel Carloto</a>
       </p>
     </footer>
   );
